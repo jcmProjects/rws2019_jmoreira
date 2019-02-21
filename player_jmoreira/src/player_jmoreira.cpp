98a91887511864bf6ac4a9584a49c561d65f85a4
João Moreira <jcmoreira@ua.pt>
2019-02-21 10:59:12 +0000

    second push

--- player_jmoreira/src/player_jmoreira.cpp
+++ player_jmoreira/src/player_jmoreira.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
+#include <ros/ros.h>
 
-int main() {
 
-    std::cout << "Hello World!" << std::endl;
+const int MY_VAR = 2;
 
-    return 1;
+
+struct A {
+
+    int b;
+}
+
+
+int doSomethin() {
+
+    return 0;
+}
+
+int main(int argc, char* argv[]) {
+
+    ros::init(argc, argv, "player_jmoreira");
+    // ros::Rate r(100);
+
+    for (int i=0; i<10; i++) {
+        std::cout << i+1 << std::endl;
+    }
+
+    return 0;
 }
